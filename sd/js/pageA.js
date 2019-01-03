afb7a6cf361bd33e9086d617d954d73276d0835c
zwj <469387971@qq.com>
2019-01-03 10:36:01 +0800

    场景1，开窗，场景2，女孩男孩走路

--- sd/js/pageA.js
+++ sd/js/pageA.js
@@ -1,6 +1,20 @@
 $(function(){
 	$('.boy').animate({top:'70%',left:'-30rem',width:'25rem',height:'10rem'},8000)
 	setTimeout(function(){$('.boy').addClass('boy-right');},8000);
-	setTimeout(function(){$('.boy').removeClass('boy-move');},12000);
+	setTimeout(function(){
+		$('.boy').removeClass('boy-move');
+		setTimeout(function(){
+			var page = document.getElementById("page");
+			page.value = "page-b";
+			if("createEvent" in document){
+				var evt = document.createEvent("HTMLEvents");
+				evt.initEvent("change",false,true);
+				page.dispatchEvent(evt);
+			}else{
+				page.fireEvent("onchange");
+			}
+			console.log(page);
+		},2000);
+	},12000);
 	$('.boy').animate({top:'80%',left:'65rem',width:'40rem',height:'16rem'},4000)
 })
--- sd/js/pageB.js
+++ sd/js/pageB.js
@@ -1,10 +1,13 @@
-function pageB (callback) {
-    
-
-    //模拟执行时间
-	setTimeout(function() {
-
-		callback()
-
-	}, 1000)
-}
+$(function(){
+	setTimeout(function(){
+		$(".boy1").animate({right:"20rem"},2500)
+		setTimeout(function(){
+			$(".girl")[0].style.animation = "gm 2s 0s steps(8) forwards";
+			$(".boy1").removeClass("boy-walk");
+			$(".boy1").addClass("boy-stand");
+			console.log('test');
+		},2500)
+	},22000)
+	
+	
+})
